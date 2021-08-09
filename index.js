edc9b6ef103c7192e3f17bf0b4d7f2dbd008de16
kalunge <muthomititus+github@gmail.com>
2021-08-09 15:39:03 +0100

    Add Js logic for persisting data in the local storage

--- index.js
+++ index.js
@@ -1,51 +1,75 @@
 // Add a book collection
-let books = [
-  {
-    id: 1,
-    title: 'The River Between',
-    author: 'Ngugi Wa Thiongo',
-  },
-  {
-    id: 2,
-    title: 'Half of a yellow sun ',
-    author: 'Chimamanda Ngozi Adichie ',
-  },
-];
+let books = [];
 
-// Method to addBook
-
-const addBook = (book) => {
-  books.push(book);
-};
-
-const book = {
-  id: 3,
-  title: 'Things fall apart',
-  author: 'Chinua Achebe',
-};
-
-addBook(book);
-
-
-// // REmove a book
-const removeBook = (id) => {
-  books = books.filter((book) => {
-    return book.id !== id;
-  });
-};
+if (localStorage.getItem('books') === null) {
+  localStorage.setItem('books', JSON.stringify(books));
+} else {
+  retrievedBooks = JSON.parse(localStorage.getItem('books'));
+}
 
 // displaying books
-books.forEach((book) => {
-  const parentDiv = document.querySelector('.books');
-  const div = document.createElement('div');
-  div.classList.add('book');
-  div.innerHTML = `
+const displayBooks = () => {
+  retrievedBooks.forEach((book) => {
+    const parentDiv = document.querySelector('.books');
+    const div = document.createElement('div');
+    div.classList.add('book');
+    div.innerHTML = `
     <h5>${book.title}</h5>  
     <p>${book.author}</p>  
-    <button>remove</button>
+    <button class="remove">remove</button>
     <hr>
   `;
 
-  parentDiv.appendChild(div);
-});
+    parentDiv.appendChild(div);
+  });
+};
 
+const addButton = document.querySelector('#add-button');
+const addBook = (e) => {
+  e.preventDefault();
+  let title = document.querySelector('#title').value;
+  let author = document.querySelector('#author').value;
+
+  const book = {
+    title,
+    author,
+  };
+
+  book.id = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+  retrievedBooks.push(book);
+  localStorage.setItem('books', JSON.stringify(retrievedBooks));
+  document.querySelector('#title').value = '';
+  document.querySelector('#author').value = '';
+  const parentDiv = document.querySelector('.books');
+  const div = document.createElement('div');
+  div.classList.add('book');
+  div.dataset.id = book.id;
+  div.innerHTML = `
+    <h5>${book.title}</h5>  
+    <p>${book.author}</p>  
+    <button class="remove">remove</button>
+    <hr>
+  `;
+  parentDiv.appendChild(div);
+};
+
+const removeBook = (elem) => {
+  if (elem.classList.contains('remove')) {
+    elem.parentElement.remove();
+  }
+};
+
+document.addEventListener('DOMContentLoaded', displayBooks);
+
+addButton.addEventListener('click', addBook);
+document.querySelector('.books').addEventListener('click', (e) => {
+  removeBook(e.target);
+  const newBooks = JSON.parse(localStorage.getItem('books'));
+  newBooks.forEach((book, index) => {
+    if (book.author === e.target.previousElementSibling.textContent) {
+      newBooks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('books', JSON.stringify(newBooks));
+});
