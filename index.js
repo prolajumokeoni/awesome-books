943478ddfa952708c70660eace9d87d8c08589e2
kalunge <muthomititus+github@gmail.com>
2021-08-09 11:33:33 +0100

    Add Js logic for removing a book

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+// Add a book collection
+const books = [
+  {
+    id: 1,
+    title: 'The River Between',
+    author: 'Ngugi Wa Thiongo',
+  },
+  {
+    id: 2,
+    title: 'Half of a yellow sun ',
+    author: 'Chimamanda Ngozi Adichie ',
+  },
+];
 
+// Method to addBook
+
+const addBook = (book) => {
+  books.push(book);
+};
+
+const book = {
+  id:3,
+  title: 'Things fall apart',
+  author: 'Chinua Achebe',
+};
+
+addBook(book)
+
+
+// REmove a book
+
+const removeBook = (id) => {
+  return books.filter((book) => {
+    book.id !== id;
+  });
+};
+
+
+console.log(books);
